3fa8bcff40359803fc9980687707acc09dcecd3c
JeongP <wjdvy0409@naver.com>
2021-02-21 02:25:23 +0900

    Add front Layout

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,10 @@ import Footer from "./components/Footer";
 import { Switch, Route, withRouter, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import './App.css';
+import Article from "./pages/Article";
+import Login from "./pages/Login";
+import Settings from "./Settings";
+import Editor from "./pages/Article/Editor";
 
 
 function App() {
@@ -12,13 +16,13 @@ function App() {
       <Header />
       <BrowserRouter>
         <Switch>
-          {/* <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/editor/:slug?" component={Editor} />
-          <Route path="/article/:id" component={Article} />
-          <PrivateRoute path="/settings" component={Settings} />
-          <Route path="/@:username" component={Profile} />
-          <Route path="/@:username/favorites" component={Profile} /> */}
+          <Route path="/login" component={Login} />
+          {/* <Route path="/register" component={Register} /> */}
+          <Route path="/editor" component={Editor} />
+          <Route path="/article/" component={Article} />
+          <Route path="/settings" component={Settings} />
+          {/* <Route path="/@:username" component={Profile} /> */}
+          {/* <Route path="/@:username/favorites" component={Profile} /> */}
           <Route path="/" component={Home} />
         </Switch>
       </BrowserRouter>
